9350da9d4e429b6acfaa7ac0f0a53d7f7d3086ea
Leonardo Wistuba de França <leonardowistuba@gmail.com>
2016-10-06 22:28:44 -0300

    Adiciona projeto base

---
+++ dojo-day/dojo-day/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dojo-day
+//
+//  Created by Leonardo Wistuba de França on 10/6/16.
+//  Copyright © 2016 GuiaBolso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ dojo-day/dojo-dayTests/Tests.swift
@@ -0,0 +1,25 @@
+//
+//  Tests.swift
+//  dojo-day
+//
+//  Created by Leonardo Wistuba de França on 10/6/16.
+//  Copyright © 2016 GuiaBolso. All rights reserved.
+//
+
+import XCTest
+
+class Tests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testExample() {
+        XCTAssert(false)
+    }
+    
+}
